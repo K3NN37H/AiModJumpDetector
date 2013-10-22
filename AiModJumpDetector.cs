28bb1746272b401a42dabcdbdc572a46a348688c
K3NN37H <kjtchan@gmail.com>
2013-10-22 15:46:16 -0400

    Initial files

---
+++ AiModJumpDetector.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using osu.GameModes.Edit.AiMod;
+using osu.GameplayElements.HitObjects;
+using osu.GameModes.Edit.AiMod.Reports;
+
+namespace AiModJumpDetector
+{
+    /// <summary>
+    /// Simple plugin that attempts to detect for jumps in a beatmap.
+    /// </summary>
+    public class AiModJumpDetector : AiModRuleset
+    {
+        public override AiModType Type
+        {
+            get { return AiModType.Compose; }
+        }
+
+        protected override void RunAllRules(List<HitObjectBase> hitObjects)
+        {
+            //Do your processing in here
+            Reports.Add(new AiReport(Severity.Info, "Jump Detector loaded!"));
+            HitObjectBase prev = null;
+            double prevSpeed = -1;
+            int count = 0;
+            foreach (HitObjectBase h in hitObjects)
+            {
+                HitObjectType temp = h.Type;
+                if ((temp & HitObjectType.Spinner) == HitObjectType.Spinner)
+                {
+                    if (h.Length < 1000)
+                    {
+                        Reports.Add(new AiReport(h.StartTime, Severity.Warning, "Spinner is less than 1000ms", 0, null));
+                    }
+                }
+
+                if (prev != null)
+                {
+                    if ((temp & HitObjectType.Spinner) == HitObjectType.Spinner)
+                    {
+                        prev = null;
+                        prevSpeed = -1;
+                        continue;
+                    }
+                    int timeBet = h.StartTime - prev.EndTime;
+                    double disX = (Math.Pow(h.PositionAtTime(h.StartTime).X - prev.EndPosition.X, 2.0));
+                    double disY = (Math.Pow(h.PositionAtTime(h.StartTime).Y - prev.EndPosition.Y, 2.0));
+                    double dis = (Math.Sqrt(disX + disY));
+                    if (dis < 8) // prevents stacked notes from causing jump detection
+                    {
+                        prev = h;
+                        continue;
+                    }                  
+                    double speed = dis / timeBet;
+                    if (prevSpeed > -1)
+                    {
+                        if (speed > prevSpeed * 1.5) // sensitivity for what counts as a jump here
+                        {
+                            Reports.Add(new AiReportTwoObjects(prev, h, null, Severity.Info, "Jump detected", 0));
+                            count += 1;
+                        }                       
+                    }
+                    prevSpeed = speed;
+                }
+                prev = h;
+            }
+            Reports.Add(new AiReport(Severity.Info, count + " jumps detected"));
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using osu.GameModes.Edit.AiMod;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AiModJumpDetector")]
+[assembly: AssemblyDescription("Detects jumps in a beatmap")]
+[assembly: AssemblyProduct("AiModJumpDetector")]
+[assembly: AssemblyCopyright("")]
+
+//THIS LINE IS IMPORTANT! You need to include your assembly name (which should match the filename of your dll) and the Ruleset you wish to load.
+[assembly: AiModRulesetAttribute("AiModJumpDetector", "AiModJumpDetector.AiModJumpDetector")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
